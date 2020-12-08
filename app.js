566e8e902987da33d4105cd4923beeee0c61766a
jiamingjun <jmj_yc@163.com>
2020-12-08 14:33:58 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello I am leanning aws!'));
+app.get('/', (req, res) => res.send('Hello I am leanning aws, xin yuan!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
